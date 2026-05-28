82f5987a1a5b18dce62e6e1445f52ecb9ff0e4a5
Heer Patel <heerpatel0615@gmail.com>
2026-05-28 23:51:34 +0530

    Add files via upload

---
+++ first_script.py
@@ -0,0 +1,8 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("heer")
+heer
+>>> print("17")
+17
+>>> print("my school name is divine public school")
+my school name is divine public school
